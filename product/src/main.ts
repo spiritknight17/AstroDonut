2f44ac6e38f21cab2ceef4f54267ebbc927167de
Barusu <payormatthewjoshguerrero@gmail.com>
2025-10-26 14:57:12 +0800

    First layer of Check verified

--- product/src/main.ts
+++ product/src/main.ts
@@ -1,6 +1,15 @@
 import { bootstrapApplication } from '@angular/platform-browser';
+import { provideHttpClient } from '@angular/common/http';
+import { AppComponent } from './src/app/app.component';
 import { appConfig } from './app/app.config';
 import { App } from './app/app';
+import { provideRouter } from '@angular/router';
+import { routes } from './src/app/app-routing.module';
 
-bootstrapApplication(App, appConfig)
-  .catch((err) => console.error(err));
+bootstrapApplication(AppComponent, {
+  providers: [
+    provideRouter(routes),
+    provideHttpClient()
+  ]
+})
+  .catch((err: unknown) => console.error(err));
